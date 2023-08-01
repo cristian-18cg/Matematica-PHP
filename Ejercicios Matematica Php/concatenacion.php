e1513a25bf384100cab0e1c2a469545014c39fe8
cristian-18cg <104658855+cristian-18cg@users.noreply.github.com>
2023-07-31 20:49:07 -0500

    Add files via upload

---
+++ Ejercicios Matematica Php/concatenacion.php
@@ -0,0 +1,12 @@
+<?php
+
+$Parte1= "El hotel del centro es el más antiguo del pueblo ";
+$Parte2 = "y también es aquel que tiene";
+$Parte3 ="más comodidades. Este hotel fue construido en 1911";
+
+$Parrafofinal = $Parte1 . $Parte2 . $Parte3;  
+
+echo"<p>". $Parrafofinal ."</p>"; 
+
+
+?>
